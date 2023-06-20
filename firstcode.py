6624cbf3d5c7db47d89cf3ea423d7bfc62d247bc
Shomarri Romell Diaz <111364800+sdcloud92@users.noreply.github.com>
2023-06-20 15:57:21 +0100

    Add files via upload

---
+++ firstcode.py
@@ -0,0 +1,11 @@
+#Lab7:
+
+NBAMostValuablePlayerAward = {
+    2019: "Kawhi Leonard",
+    2020: "Lebron James",
+    2021: "Giannis Antetokoumpo",
+    2022: "Stephen Curry",
+    2023: "Nikola Jokic"
+}
+
+print(NBAMostValuablePlayerAward.copy())
